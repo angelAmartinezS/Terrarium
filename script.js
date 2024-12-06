a6f8b0dde2bf34ab74e5b50f8b1bba7a5aa37627
angelAmartinezS <l221130268@gmail.com>
2024-12-05 19:03:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,27 +14,25 @@ dragElement(document.getElementById('plant13'));
 dragElement(document.getElementById('plant14'));
 
 function dragElement(terrariumElement) {
-  // Estableciendo las posiciones
-  // Estableciendo posiciones iniciales
-  let despX = 0, despY = 0, initPosX = 0, initPosY = 0;
-  // Se le asigna una función al evento
-  terrariumElement.onpointerdown = pointerDrag;
-  // Función que detecta la seleccion
-	function pointerDrag(event){
-	  // Se previene el 
-	  // funcionamiento por defecto
-	  // Un fantasma de arrastre
-	  event.preventDefault();
-	  // Se accedede a la posicion del mouse
-    // mediante las propiedades clientX y clientY
-    // del evento
-    initPosX = event.clientX;
-    initPosY = event.clientY;
-    // Se crea la funcion de arrastre
-    // dentro del estado pointerDrag
-	  document.onpointermove = elementDrag;
-	  // Se crea la funcion de soltar
-    // dentro del estado pointerDrag
-	  document.onpointerup = stopElementDrag;
-	}
+  function dragElement(terrariumElement) {
+  // Definición de la función de arrastre
+  function elementDrag(eventOnPointerMove) {
+    // se calcula una nueva posición
+    // como la diferencia de la posición guardada
+    // en las variables pos3 y pos4
+    despX = eventOnPointerMove.clientX - initPosX;
+    despY = eventOnPointerMove.clientY - initPosY;
+    // Se actualiza la posicion inicial del mouse
+    initPosX = eventOnPointerMove.clientX;
+    initPosY = eventOnPointerMove.clientY;
+    // Se imprimen las posiciones
+    console.log(despX, despY, initPosX, initPosY);;
+    // Se le aplica offset al elemento, es decir se desplaza a la nueva
+    // posicion del mouse
+    terrariumElement.style.left = `${terrariumElement.offsetLeft + despX}px`;
+    terrariumElement.style.top = `${terrariumElement.offsetTop + despY}px`;
+	  function stopElementDrag() {
+    document.onpointerup = null;
+    document.onpointermove = null;
+  }
 }
